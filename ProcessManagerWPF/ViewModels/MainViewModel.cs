7b699138c06da89fcb2e64ba06f88331742b9ed7
Александр Макухин <sasamakuxin5@gmail.com>
2026-02-24 12:13:15 +0300

    Add search functionality and Refresh command (MVVM with ICommand)

--- ProcessManagerWPF/ViewModels/MainViewModel.cs
+++ ProcessManagerWPF/ViewModels/MainViewModel.cs
@@ -7,6 +7,8 @@ using System.Collections.ObjectModel;
 using System.Windows.Threading;
 using ProcessManagerWPF.Models;
 using ProcessManagerWPF.Services;
+using System.Windows.Input;
+using ProcessManagerWPF.Utilities;
 
 namespace ProcessManagerWPF.ViewModels
 {
@@ -15,17 +17,38 @@ namespace ProcessManagerWPF.ViewModels
         private readonly ProcessService _processService;
         private readonly DispatcherTimer _timer;
 
+        private string _searchText;
+
         public ObservableCollection<ProcessInfo> Processes { get; set; }
+        private ObservableCollection<ProcessInfo> _allProcesses;
+
+        public ICommand RefreshCommand { get; }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+                ApplyFilter();
+            }
+        }
 
         public MainViewModel()
         {
             _processService = new ProcessService();
             Processes = new ObservableCollection<ProcessInfo>();
+            _allProcesses = new ObservableCollection<ProcessInfo>();
+
+            RefreshCommand = new RelayCommand(_ => LoadProcesses());
 
             LoadProcesses();
 
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3)
+            };
             _timer.Tick += (s, e) => LoadProcesses();
             _timer.Start();
         }
@@ -34,11 +57,25 @@ namespace ProcessManagerWPF.ViewModels
         {
             var processList = _processService.GetAllProcesses();
 
-            Processes.Clear();
+            _allProcesses.Clear();
             foreach (var process in processList)
-            {
+                _allProcesses.Add(process);
+
+            ApplyFilter();
+        }
+
+        private void ApplyFilter()
+        {
+            Processes.Clear();
+
+            var filtered = string.IsNullOrWhiteSpace(SearchText)
+                ? _allProcesses
+                : new ObservableCollection<ProcessInfo>(
+                    _allProcesses.Where(p =>
+                        p.Name.ToLower().Contains(SearchText.ToLower())));
+
+            foreach (var process in filtered)
                 Processes.Add(process);
-            }
         }
     }
 }
